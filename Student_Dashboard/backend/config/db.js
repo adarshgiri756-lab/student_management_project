19708e41f00a27413ccfdc05ec205248f5c4e49b
adarshgiri756-lab <adarshgiri756@gmail.com>
2026-07-02 11:13:36 +0530

    made changes in database

--- Student_Dashboard/backend/config/db.js
+++ Student_Dashboard/backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect("mongodb+srv://adarshgiri756_db_user:U2oAOqWHtivSOHJK@cluster0.nfkji0k.mongodb.net/?appName=Cluster0/MERN");
+    const connection = await mongoose.connect("mongodb+srv://adarshgiri756_db_user:U2oAOqWHtivSOHJK@cluster0.nfkji0k.mongodb.net/?appName=Cluster0/MERN",{
+      dbName:"MERN"
+    });
     console.log(`MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection failed: ${error.message}`);
